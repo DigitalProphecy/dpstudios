78b9a9fb39c61feca5c806e68b7b0251d970087c
Darryl Nicholson <darryl.a.nicholson@gmail.com>
2023-08-14 22:27:24 -0700

    version uptick

--- app/themes/dps-theme/functions.php
+++ app/themes/dps-theme/functions.php
@@ -23,7 +23,7 @@ use DPS\App\Fields\FieldGroups\PageBuilderFieldGroup;
  * Define Theme Version
  * Define Theme directories
  */
-define('THEME_VERSION', '1.0.7');
+define('THEME_VERSION', '1.0.8');
 define('DPS_THEME_DIR', trailingslashit(get_template_directory()));
 define('DPS_THEME_PATH_URL', trailingslashit(get_template_directory_uri()));
 
